21121410fe6c624c304dd1641abeccb9fa45741c
shubham kumar <shubham,k@cwsinfotech.com>
2022-07-03 11:03:27 +0530

    added script

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
+let config = {
+    phone :"971529910891",
+    call :"Message Us",
+    position :"ww-right",
+    size : "ww-normal",
+    text : "",
+    type: "ww-extended",
+    brand: "",
+    subtitle: "",
+    welcome: ""
+    };
 class MyDocument extends Document {
+    
     render() {
         return (
-            <Html lang="zxx">
+            <Html lang="english">
                 <Head>
                     <link
                         href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
@@ -14,25 +25,7 @@ class MyDocument extends Document {
                         type="image/png"
                         href="/images/favicon.ico"
                     ></link>
-                    <script type="text/javascript">
-                        {`var config = {
-                            phone :" 971529910891",
-                        call :"Message Us",
-                        position :"ww-right",
-                        size : "ww-normal",
-                        text : "",
-                        type: "ww-extended",
-                        brand: "",
-                        subtitle: "",
-                        welcome: ""
-    };
-                        var proto = document.location.protocol, host = "cloudfront.net", url = proto + "//d3kzab8jj16n2f." + host;
-                        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true; s.src = url + "/v2/main.js";
-
-                        s.onload = function () {tmWidgetInit(config)};
-                        var x = document.getElementsByTagName("script")[0]; x.parentNode.insertBefore(s, x);
-                    `}
-                    </script>
+                     <script type="text/javascript" src="/js/whatsapp_script.js"></script>
                 </Head>
                 <body>
                     <Main />
