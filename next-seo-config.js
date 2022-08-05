d8ef37b95ec95532a5c49f986da1cf32ec2c2f1d
Mintu Bisht <m.bisht0805@gmail.com>
2022-08-05 20:12:38 +0530

    added

--- next-seo-config.js
+++ next-seo-config.js
@@ -6,20 +6,20 @@ module.exports  = {
         url: 'http://www.cwstechnology.com/',
         title: "Cws technology",
         description: "CWS Technology is the best and leading web development company in India. We are among the top web development agency who provide website development services.",
-        images: [
-            {
-              url: '/images/banner-two/banner_main.jpg',
-              width: 800,
-              height: 600,
-              alt: 'Og Image Alt',
-            },
-            {
-              url: '/images/banner-two/banner_main.jpg',
-              width: 800,
-              height: 600,
-              alt: 'Og Image Alt 2',
-            },
-          ],
+        // images: [
+        //     {
+        //       url: '/images/banner-two/banner_main.jpg',
+        //       width: 800,
+        //       height: 600,
+        //       alt: 'Og Image Alt',
+        //     },
+        //     {
+        //       url: '/images/banner-two/banner_main.jpg',
+        //       width: 800,
+        //       height: 600,
+        //       alt: 'Og Image Alt 2',
+        //     },
+        //   ],
     },
     twitter: {
         handle: '@cwstechnology',
