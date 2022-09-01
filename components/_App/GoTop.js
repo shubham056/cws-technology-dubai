94a86c19764204d03e83d55855f9a1b4ad462d0c
Mintu Bisht <m.bisht0805@gmail.com>
2022-09-01 12:08:05 +0530

    asa

--- components/_App/GoTop.js
+++ components/_App/GoTop.js
@@ -60,7 +60,7 @@ const GoTop = ({scrollStepInPx, delayInMs}) => {
                     .go-top.active {
                         opacity: 1;
                         visibility: visible;
-                        bottom: 104px !important;
+                        bottom: 100px !important;
                     }
                     .go-top:hover {
                         background-color: var(--main-color);
